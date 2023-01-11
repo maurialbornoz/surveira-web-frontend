ec1450feae98efd8c0aefbe996924592bd1595d3
mauri300 <mauriciojose1999alb@gmail.com>
2023-01-11 09:58:33 -0300

    backend connection config

--- src/utils/endpoints.ts
+++ src/utils/endpoints.ts
@@ -1,6 +1,7 @@
 import { USER_POLLS_PER_PAGE } from "./constants";
 
-const ENDPOINT_URL = "https://surveira-web-backend-production.up.railway.app";
+// const ENDPOINT_URL = "https://surveira-web-backend-production.up.railway.app";
+const ENDPOINT_URL = "https://surveira-backend.onrender.com";
 
 // const ENDPOINT_URL = "http://localhost:8080";
 
